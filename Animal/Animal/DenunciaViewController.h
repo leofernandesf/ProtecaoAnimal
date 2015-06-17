a66d1aa55a9c3ac0d16fa01eb2d34978a2fb3716
Pedro Luis Berbel dos Santos <pedroberbel@Pedro-Berbel.local>
2015-06-17 11:17:53 -0400

    DenunciaViewController recriada some teclado quando clicar na tela [ok] sobe campos quando for digitar [ok]

--- Animal/Animal/DenunciaViewController.h
+++ Animal/Animal/DenunciaViewController.h
@@ -2,13 +2,16 @@
 //  DenunciaViewController.h
 //  Animal
 //
-//  Created by Pedro Luis Berbel dos Santos on 10/06/15.
+//  Created by Pedro Luis Berbel dos Santos on 17/06/15.
 //  Copyright (c) 2015 leonardo fernandes farias. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DenunciaViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+@interface DenunciaViewController : UIViewController <UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UITextField *tipo;
+@property (strong, nonatomic) IBOutlet UITextField *descricao;
+@property (strong, nonatomic) IBOutlet UITextField *referencia;
+- (IBAction)foto:(id)sender;
 
-@property (strong, nonatomic) IBOutlet UIPickerView *picker;
 @end
--- Animal/Animal/DenunciaViewController.m
+++ Animal/Animal/DenunciaViewController.m
@@ -2,7 +2,7 @@
 //  DenunciaViewController.m
 //  Animal
 //
-//  Created by Pedro Luis Berbel dos Santos on 10/06/15.
+//  Created by Pedro Luis Berbel dos Santos on 17/06/15.
 //  Copyright (c) 2015 leonardo fernandes farias. All rights reserved.
 //
 
@@ -12,16 +12,32 @@
 
 @end
 
-@implementation DenunciaViewController 
+@implementation DenunciaViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
+    
     
     // teclado some quando clicar na tela
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
     tap.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:tap];
     
+    
+    //pra subir os campos quando o teclado aparece:
+    self.tipo.layer.borderColor=[[UIColor colorWithRed:1 green:1 blue:1 alpha:1]CGColor];
+    self.tipo.layer.borderWidth=0;
+    self.tipo.delegate = self;
+    
+    self.descricao.layer.borderColor=[[UIColor colorWithRed:1 green:1 blue:1 alpha:1]CGColor];
+    self.descricao.layer.borderWidth=0;
+    self.descricao.delegate = self;
+    
+    self.referencia.layer.borderColor=[[UIColor colorWithRed:1 green:1 blue:1 alpha:1]CGColor];
+    self.referencia.layer.borderWidth=0;
+    self.referencia.delegate = self;
+    //fim
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,9 +54,6 @@
     // Pass the selected object to the new view controller.
 }
 */
-/*
- ATIVAR ESTA PARTE QUANDO A TELA ESTIVER PRONTA!!!!
- 
 //some teclado, verificar
 -(BOOL)textFieldShouldReturn:(UITextField *)textField{
     [textField resignFirstResponder];
@@ -49,10 +62,40 @@
 
 
 - (void)dismissKeyboard {
-    [self. resignFirstResponder];
-    [self.segundoNome resignFirstResponder];
-    [self.email resignFirstResponder];
-    [self.senha resignFirstResponder];
+    [self.tipo resignFirstResponder];
+    [self.descricao resignFirstResponder];
+    [self.referencia resignFirstResponder];
+    
 }
-*/
+//pra subir os campos quando o teclado aparece:
+- (void)textViewDidBeginEditing:(UITextView *)textView{
+    [self animate:YES];
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView{
+    [self animate:NO];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [self animate:YES];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [self animate:NO];
+}
+
+- (void) animate: (BOOL)up {
+    const int movementDistance = 80; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    [UIView beginAnimations: @"anim" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
+
+- (IBAction)foto:(id)sender {
+}
 @end
