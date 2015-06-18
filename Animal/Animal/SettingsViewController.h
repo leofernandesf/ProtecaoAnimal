7223b6e40c4ce77cbba41e48d1746b865dc075f8
Pedro Luis Berbel dos Santos <pedroberbel@Pedro-Berbel.local>
2015-06-18 09:26:17 -0400

    criaçao tela de configuraçao: SettingsViewController

---
+++ Animal/Animal/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Animal
+//
+//  Created by Pedro Luis Berbel dos Santos on 18/06/15.
+//  Copyright (c) 2015 leonardo fernandes farias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
