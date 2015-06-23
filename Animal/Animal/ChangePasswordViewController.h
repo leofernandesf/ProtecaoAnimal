0a959096c53325e9f4ee9f3aebd6960f535993a9
Pedro Luis Berbel dos Santos <pedroberbel@Pedro-Berbel.local>
2015-06-23 00:45:06 -0400

    Tela de Redefinir Senha criada; classe ChangePasswordViewController criada.

---
+++ Animal/Animal/ChangePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewController.h
+//  Animal
+//
+//  Created by Pedro Luis Berbel dos Santos on 23/06/15.
+//  Copyright (c) 2015 leonardo fernandes farias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangePasswordViewController : UIViewController
+
+@end
