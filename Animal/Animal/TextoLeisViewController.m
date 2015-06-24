6c4d694a2abb05262180cf3cacb36f416935a1d8
Pedro Luis Berbel dos Santos <pedroberbel@Pedro-Berbel.local>
2015-06-24 08:26:54 -0400

    alterada tela de denúncia para o novo padrão

---
+++ Animal/Animal/TextoLeisViewController.m
@@ -0,0 +1,31 @@
+//
+//  TextoLeisViewController.m
+//  Animal
+//
+//  Created by Pedro Luis Berbel dos Santos on 23/06/15.
+//  Copyright (c) 2015 leonardo fernandes farias. All rights reserved.
+//
+
+#import "TextoLeisViewController.h"
+
+@interface TextoLeisViewController ()
+
+@end
+
+@implementation TextoLeisViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+_textoLeis = @[@"vilar", @"antonio", @"guto"];
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
