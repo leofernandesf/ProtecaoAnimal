e2e6e3020d599cad7a94d0e724707f7d5d8c270a
Pedro Luis Berbel dos Santos <pedroberbel@Pedro-Berbel.local>
2015-06-25 15:30:22 -0400

    Alteradas as fontes das telas de informaçoes : DEMA e LEIS - Sofia pro Light

--- Animal/Animal/DescricaoLeisViewController.m
+++ Animal/Animal/DescricaoLeisViewController.m
@@ -27,7 +27,8 @@
     
     _lbDescricao.text = self.descricao;
     _lbDescricao.textAlignment = NSTextAlignmentJustified;
-    [_lbDescricao setFont:[UIFont boldSystemFontOfSize:20]];
+  //  [_lbDescricao setFont:[UIFont boldSystemFontOfSize:20]];
+    [_lbDescricao setFont:[UIFont fontWithName:@"SofiaProLight" size: 15]];
     
 }
 
